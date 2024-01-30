be1384be4da15ed07879f1c7a72617ad92592912
Samanja Cartagena <c.samanja09@Pabs.local>
2024-01-30 12:29:34 -0500

    debugging tool created

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def calculate():
+    x =1 
+    y = 2
+    return x
+
 
 def say_hello(request):
-    return render(request,'hello.html')
+    x = calculate()
+    return render(request,'hello.html',{
+        'name':'Mosh'
+    })
